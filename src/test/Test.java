a16c952020b830a7c41ef62ce0cbfb8b923ad8f7
rembau <504229406@qq.com>
2013-01-23 22:55:38 +0800

    ff

--- src/test/Test.java
+++ src/test/Test.java
@@ -2,9 +2,6 @@ package test;
 
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("fd");
-		//
-		System.out.println("eee");
-		//
+		System.out.println("www");
 	}
 }
